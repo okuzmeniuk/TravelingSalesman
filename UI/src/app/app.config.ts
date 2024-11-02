a11ecb297f31f741b198418e974c0135ebb0c476
Oleksandr Kuzmeniuk <oleksandrkuzmenuk2@gmail.com>
2024-11-02 12:47:37 +0200

    Added styles to login and register components

--- UI/src/app/app.config.ts
+++ UI/src/app/app.config.ts
@@ -4,11 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { jwtInterceptor } from './auth/jwt.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([jwtInterceptor]))
+    provideHttpClient(withInterceptors([jwtInterceptor])), provideAnimationsAsync()
   ]
 };
